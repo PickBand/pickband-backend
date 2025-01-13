5547d5fc27f7a598da54c918f88acd04f17a7f31
JinGoo <zzgg3040@naver.com>
2025-01-14 02:21:31 +0900

    first commit

---
+++ src/main/java/org/pickband/PickBandApplication.java
@@ -0,0 +1,13 @@
+package org.pickband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickBandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickBandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pickband/PickBandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pickband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickBandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
